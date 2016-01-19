b49f10b47791425b6b44470c4c7bbdc6efe347c2
etmoore <etmoore@gmail.com>
2016-01-19 13:31:48 -0700

    create abstract-functions for assessing students ability to abstract code into a function

---
+++ abstract-functions.js
@@ -0,0 +1,34 @@
+// Clean up the code below with a single function called `personalIntroduction`
+// The new code should have the exact same functionality and output.
+
+var name = "Bob";
+var height = "72";
+var favoriteColor = "red";
+var string = "My name is " + name;
+string += ", and my height is " + height;
+string += ", and my favorite color is " + favoriteColor;
+console.log(string);
+
+var name = "Samantha";
+var height = "65";
+var favoriteColor = "orange";
+var string = "My name is " + name;
+string += ", and my height is " + height;
+string += ", and my favorite color is " + favoriteColor;
+console.log(string);
+
+var name = "Christopher";
+var height = "69";
+var favoriteColor = "green";
+var string = "My name is " + name;
+string += ", and my height is " + height;
+string += ", and my favorite color is " + favoriteColor;
+console.log(string);
+
+var name = "Tamera";
+var height = "63";
+var favoriteColor = "blue";
+var string = "My name is " + name;
+string += ", and my height is " + height;
+string += ", and my favorite color is " + favoriteColor;
+console.log(string);
