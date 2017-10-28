41f01d0f30dd5603058512c850a24d09cbd98df6
Thomas Palzkill <tpalzkilll@gmail.com>
2017-10-28 12:51:10 -0500

    complete

--- abstract-functions.js
+++ abstract-functions.js
@@ -1,34 +1,32 @@
 // Clean up the code below with a single function called `personalIntroduction`
 // The new code should have the exact same functionality and output.
-
-var name = "Bob";
-var height = "72";
-var favoriteColor = "red";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Samantha";
-var height = "65";
-var favoriteColor = "orange";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Christopher";
-var height = "69";
-var favoriteColor = "green";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
-
-var name = "Tamera";
-var height = "63";
-var favoriteColor = "blue";
-var string = "My name is " + name;
-string += ", and my height is " + height;
-string += ", and my favorite color is " + favoriteColor;
-console.log(string);
+function personalIntroduction() {
+  let people =
+ [
+  {
+    name: "Bob",
+    height: "72",
+    favoriteColor: "red",
+  }, {
+    name: "Samantha",
+    height: "65",
+    favoriteColor: "orange",
+  }, {
+    name: "Christopher",
+    height: "69",
+    favoriteColor: "green",
+  }, {
+    name: "Tamera",
+    height: "63",
+    favoriteColor: "blue",
+  }
+];
+for (let i = 0, i < people.length; i++) {
+   let position = people[i];
+  console.log(
+    "My name is " + position.name;
+    string += ", and my height is " + position.height;
+    string += ", and my favorite color is " + position.favoriteColor;
+  )
+}
+personalIntroduction();
